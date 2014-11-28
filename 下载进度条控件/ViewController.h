931e3c46c68f269b6a1ec19a84cec662e97e7a62
YouXianMing <705786299@qq.com>
2014-11-29 00:03:02 +0800

    Initial Commit

---
+++ 下载进度条控件/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  下载进度条控件
+//
+//  Created by YouXianMing on 14/11/29.
+//  Copyright (c) 2014年 YouXianMing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 下载进度条控件/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  下载进度条控件
+//
+//  Created by YouXianMing on 14/11/29.
+//  Copyright (c) 2014年 YouXianMing. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
