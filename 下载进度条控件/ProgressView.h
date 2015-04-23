7da64c8ddd05b218b91360b5123764d78841b168
zhangyong <c99777@163.com>
2015-04-23 15:19:27 +0800

    first commit

---
+++ 下载进度条控件/ProgressView.h
@@ -0,0 +1,13 @@
+//
+//  ProgressView.h
+//  下载进度条控件
+//
+//  Created by YouXianMing on 14/11/29.
+//  Copyright (c) 2014年 YouXianMing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProgressView : UIView
+
+@end
---
+++ 下载进度条控件/ProgressView.m
@@ -0,0 +1,21 @@
+//
+//  ProgressView.m
+//  下载进度条控件
+//
+//  Created by YouXianMing on 14/11/29.
+//  Copyright (c) 2014年 YouXianMing. All rights reserved.
+//
+
+#import "ProgressView.h"
+
+@implementation ProgressView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
